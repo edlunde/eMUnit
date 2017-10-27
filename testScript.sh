3b4b1373a471a7d27b981d744c1961eccadca67b
Erik Edlund <edlunde@gmail.com>
2017-10-27 11:15:20 -0400

    Removed Mathematica 9 bug workaround from testScript.sh

--- testScript.sh
+++ testScript.sh
@@ -1,10 +1,4 @@
-#!/bin/bash
-# MathematicaScript and MathKernel 9 have a bug that prevents output sent
-# from a Mathematica script to stdout from being catched on a pipe or
-# redirected to a file. This workaround runs the script inline into a 
-# MathKernel session.
-/Applications/Mathematica.app/Contents/MacOS/MathKernel -noprompt -run "commandLine={${1+\"$1\"}}; $(sed '1,/^exit/d' $0) ; Exit[]"
-exit $?
+#!/Applications/Mathematica.app/Contents/MacOS/MathematicaScript -script
 
 Get[DirectoryName[$InputFileName] <> "eMUnit.m"];
 Print/@ eMUnit`TestEMUnitPackage[][[1,2;;]];
